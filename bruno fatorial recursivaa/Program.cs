b9e984790bf6e179980d39e66ee10f7efab5c200
ThyagoMariano <74057836+ThyagoMariano@users.noreply.github.com>
2021-05-22 19:16:38 -0300

    pratica

---
+++ bruno fatorial recursivaa/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace bruno_fatorial_recursivaa
+{
+    class Program
+    {
+        public static int fatorial (int n)
+        {
+            if (n==0)
+            {
+                return 1;
+            }
+            else
+            {
+                return n*fatorial(n-1);
+            }    
+            
+        }
+        public static void Main(string[] args)
+        {
+            int n=0;
+            Console.WriteLine("Informe o valor de n: ");
+            n=int.Parse(Console.ReadLine());            
+            Console.WriteLine(fatorial(n));
+        }
+    }
+}
---
+++ bruno fibonacci recursiva/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace bruno_fibonacci_recursiva
+{
+    class Program
+    {
+        public static int fibonacci(int n)
+        {
+            if (n == 1 || n == 2)
+            {
+                return 1;
+            }
+            else
+            {
+                return fibonacci(n - 1) + fibonacci(n - 2);
+            }
+
+        }
+        public static void Main(string[] args)
+        {
+            int n = 0;
+            Console.WriteLine("Informe o valor de n: ");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine(fibonacci(n));
+
+        }
+    }
+}
