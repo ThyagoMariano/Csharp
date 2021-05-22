034a5aa4692b481a39cee7b9cd4734965491d5a4
ThyagoMariano <74057836+ThyagoMariano@users.noreply.github.com>
2021-05-22 19:58:14 -0300

    Cadastra voto Imcompleto

---
+++ Urna/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Urna
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int voto = 0; int cont=0;
+            string sn;
+            do
+            {
+                Console.WriteLine("Informe o Voto: ");
+                voto = int.Parse(Console.ReadLine());
+                bool ok = false;
+
+                string explicitString1 = voto.ToString();
+
+                Urnaa urn = new Urnaa();
+
+                ok = urn.Procurar(explicitString1.Trim());
+
+                if (explicitString1 == "0000")
+                {
+                    Console.WriteLine("Voto em branco");
+                }
+                if (explicitString1 == "8888")
+                {
+                    urn.Listar();
+                }
+                if (explicitString1 == "9999")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Voto NULO");
+                }
+
+                do
+                {
+                     Console.Write("Confirmar Voto ? (S/N): ");
+                       sn = Console.ReadLine();
+                       if (sn.ToUpper() != "N" && sn.ToUpper() != "S")
+                     Console.WriteLine("Opção Inválida!");
+                  } while (sn.ToUpper() != "N" && sn.ToUpper() != "S");
+
+            } while (sn.ToUpper() == "S");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Urna/Urnaa.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Urna
+{
+    class Urnaa
+    {
+        public String voto { get; set; }
+
+
+        public bool  Procurar(string procura)
+        {
+            string linha="";
+            bool ok = false;
+
+            StreamReader ler = new StreamReader("Canditados.txt");
+            while (!ler.EndOfStream)
+            {
+                linha = ler.ReadLine();
+                
+                if (linha.ToUpper().Trim() == procura.ToString().Trim())
+                {
+                    linha = ler.ReadLine();
+                    voto = linha;
+                    Console.WriteLine(linha);
+                     
+                    ok = true;
+                    break;
+                }
+
+            }
+
+            ler.Close();
+
+            return ok;
+        }
+        public void Listar()
+        {
+
+            StreamReader ver = new StreamReader("Canditados.txt");
+
+            Console.WriteLine(ver.ReadToEnd());
+            Console.ReadKey();
+            ver.Close();
+        }
+
+    }
+
+
+}
